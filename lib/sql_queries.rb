7805109869e88270752e45233a0b434a00d5654e
tgray017 <tgray017@gmail.com>
2019-01-22 12:17:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,9 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "
+  SELECT name, MIN(age) as age FROM bears;
+  "
 end
 
 def selects_most_prominent_color_and_returns_with_count
