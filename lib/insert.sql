80e9c6484ecf3014276deb913a5bf7e6a8f3b3ce
tgray017 <tgray017@gmail.com>
2019-01-22 12:09:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender color, temperament, alive) VALUES
 ("Mr. Chocolate", 2, "M", "Brown", "Friendly", 1),
-("Mr. Chocolate", 2, "M", "Brown", "Friendly", 1),
-("Mr. Chocolate", 2, "M", "Brown", "Friendly", 1),
+("Rowdy", 2, "M", "Brown", "Friendly", 1),
+("Tabitha", 2, "M", "Brown", "Friendly", 1),
 ("Mr. Chocolate", 2, "M", "Brown", "Friendly", 1),
 ("Mr. Chocolate", 2, "M", "Brown", "Friendly", 1),
 ("Mr. Chocolate", 2, "M", "Brown", "Friendly", 1),
